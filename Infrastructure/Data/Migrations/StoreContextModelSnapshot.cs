3140ebb8fe38fb50bff28b94d17cf24f30991901
Sergii Ostapchuk <sergiiostapchuk1@gmail.com>
2023-01-14 19:27:53 -0600

    Initial Commit

---
+++ Infrastructure/Data/Migrations/StoreContextModelSnapshot.cs
@@ -0,0 +1,33 @@
+﻿using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    [DbContext(typeof(StoreContext))]
+    partial class StoreContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+#pragma warning disable 612, 618
+            modelBuilder.HasAnnotation("ProductVersion", "6.0.11");
+
+            modelBuilder.Entity("API.Entitites.Product", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("INTEGER");
+
+                    b.Property<int>("Name")
+                        .HasColumnType("INTEGER");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Products");
+                });
+#pragma warning restore 612, 618
+        }
+    }
+}
